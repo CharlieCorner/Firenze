14a72c1948963b224440d34aaa26ff4ef390220d
Charlie Corner <carloserincon.uvm@gmail.com>
2012-04-01 16:34:12 -0500

    Se creó el proyecto de NetBeans listo para comenzar a ser trabajado.

---
+++ Firenze/src/firenze/Main.java
@@ -0,0 +1,18 @@
+package firenze;
+
+/**
+ *
+ * @author Charlie Corner
+ * @author Yaz Martinez
+ * @author Esteban Chaparro
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
