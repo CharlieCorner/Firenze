d09e766fe6d581aefd4aee7bc99a994173d62d25
Charlie Corner <carloserincon.uvm@gmail.com>
2012-04-08 00:31:16 -0500

    Se arregló el nombre de la clase y se creó un método con patrón de diseño Factory para la creación de reglas dada una cadena de caracteres sin parsear. El método se encarga de parsear la línea y crear un objeto de la clase.

---
+++ Firenze/src/algoritmosdeinferencia/Regla.java
@@ -0,0 +1,46 @@
+package algoritmosdeinferencia;
+
+import java.util.List;
+
+/**
+ *
+ * @author Charlie Corner
+ */
+public class Regla {
+    private int indiceDeRegla;
+    private List<String> causantes;
+    private List<String> producidos;
+
+    public Regla() {
+    }
+    
+    public static Regla crearRegla(String lineaDeReglas){
+        // ToDo
+        return null;
+        
+    }
+
+    public void setCausantes(List<String> causantes) {
+        this.causantes = causantes;
+    }
+
+    public void setProducidos(List<String> producidos) {
+        this.producidos = producidos;
+    }
+
+    public List<String> getCausantes() {
+        return causantes;
+    }
+
+    public List<String> getProducidos() {
+        return producidos;
+    }
+
+    public int getIndiceDeRegla() {
+        return indiceDeRegla;
+    }
+
+    public void setIndiceDeRegla(int indiceDeRegla) {
+        this.indiceDeRegla = indiceDeRegla;
+    }
+}
